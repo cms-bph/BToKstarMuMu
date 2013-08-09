b3b3c4fdd6f8bce9a96a2ee6c14e5fd3e6fd1fb8
Xin Shi <xshi@xshi.org>
2013-08-09 14:29:47 +0200

    BuToKstarJPsi_7TeV_5E5_v1_run2011v0_2

--- python/mrg/__init__.py
+++ python/mrg/__init__.py
@@ -43,6 +43,7 @@ def BuToKstarJPsi_7TeV_5E5_v1_run2011v0_1(com_name):
     dstdir = os.path.join(eosbase, 'dat/ntp/mc', com_name)
     merge_root_files(srcdir, dstdir)
 
+
 def Run2011A_May10ReReco_v1_run2011v0_2(com_name):
     eosbase = '/afs/cern.ch/user/x/xshi/eos/cms/store/user/xshi/'
     srcdir = os.path.join(
@@ -50,6 +51,7 @@ def Run2011A_May10ReReco_v1_run2011v0_2(com_name):
         com_name, 'f379a209b0c58c9fe256f1d4da070fb3')
     dstdir = os.path.join(eosbase, 'dat/ntp/data', com_name)
     merge_root_files(srcdir, dstdir)
+
 
 def Run2011A_PromptReco_v5_run2011v0(com_name):
     eosbase = '/afs/cern.ch/user/x/xshi/eos/cms/store/user/xshi/'
@@ -59,8 +61,14 @@ def Run2011A_PromptReco_v5_run2011v0(com_name):
     dstdir = os.path.join(eosbase, 'dat/ntp/data', com_name)
     merge_root_files(srcdir, dstdir)
 
+
 def Run2011A_PromptReco_v6_run2011v0(com_name):
     Run2011A_PromptReco_v5_run2011v0(com_name)
 
+
 def Run2011B_PromptReco_v1_run2011v0(com_name):
     Run2011A_PromptReco_v6_run2011v0(com_name)
+
+
+def BuToKstarJPsi_7TeV_5E5_v1_run2011v0_2(com_name):
+    BuToKstarJPsi_7TeV_5E5_v1_run2011v0_1(com_name) 
