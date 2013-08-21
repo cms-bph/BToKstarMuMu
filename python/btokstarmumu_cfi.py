4077693eaa2b64ecdef33add37a5252bf98edc9a
Xin Shi <xshi@xshi.org>
2013-08-21 09:39:26 +0200

    move the main sequence out

--- python/btokstarmumu_cfi.py
+++ python/btokstarmumu_cfi.py
@@ -232,4 +232,4 @@ process.patDefaultSequence.remove(process.selectedPatJets)
 process.patDefaultSequence.remove(process.cleanPatJets)
 process.patDefaultSequence.remove(process.countPatJets)
 
-process.p = cms.Path(process.patDefaultSequence * process.localV0Candidates * process.ntuple)
+#process.p = cms.Path(process.patDefaultSequence * process.localV0Candidates * process.ntuple)
