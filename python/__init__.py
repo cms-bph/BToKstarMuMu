7da71ccab84fe54f210bf7ddbfaa4f0a0fdc737c
Xin Shi <xshi@xshi.org>
2013-10-25 16:30:43 +0200

    clean up files

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/BphAna/BToKstarMuMu_run2011v0/',1)[0])+'/cfipython/slc5_amd64_gcc434/BphAna/BToKstarMuMu_run2011v0')
